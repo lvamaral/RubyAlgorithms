13784c54097bd89b8eb7a9868e85a2fdfe70b44d
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-10-25 19:04:18 -0700

    Algos

--- Heaps/lib/problems.rb
+++ Heaps/lib/problems.rb
@@ -17,3 +17,32 @@
 #         return True
 #     else:
 #         return False
+
+# Construct a MaxStack, in JS
+# class MaxStack {
+#   constructor(){
+#     this.store = [];
+#     this.maxes = [];
+#   }
+#
+# 	push(n) {
+#     this.store.unshift(n);
+#     if (this.maxes.length === 0 || this.maxes[0] <= n) {
+#       this.maxes.unshift(n);
+#     }
+# 	}
+#
+# 	pop() {
+#     const value = this.store.shift();
+#     if (value === this.maxes[0]) {
+#       this.maxes.shift();
+#     }
+#     return value;
+# 	}
+#
+# 	max() {
+# 		return this.maxes[0];
+# 	}
+# }
+#
+# let stack = new MaxStack();
